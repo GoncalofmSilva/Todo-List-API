0a249c9a4d61b53ecf65ff0e7fdd7a12f1819cc9
Goncalo Silva <goncalofmsilva2004@gmail.com>
2026-05-08 15:54:31 +0100

    starting auth functions

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from "express"
+import authRouter from "./routes/authRoute.js"
+import todoRouter from "./routes/todoRoute.js"
 
 
 const app = express()
@@ -7,5 +9,7 @@ const app = express()
 app.use(express.json())
 
 //Routes
+app.use('/', authRouter)
+app.use('/', todoRouter)
 
 export default app
